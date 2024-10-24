f332277e594a68457bbf2dce83287bd36f404306
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2024-10-24 19:53:21 +0900

    get_capture

--- src/assistant/screenshot.py
+++ src/assistant/screenshot.py
@@ -1,8 +1,16 @@
 from typing import Tuple
 import os
-import pyautogui
+# import pyautogui
+import requests
+import json
 
 
+def get_capture(initial_point: Tuple[int, int]=(0, 0), width: int=600, height: int=600):
+    url = 'http://172.25.0.1:8000/capture'
+    params = {'init_x': initial_point[0], 'init_y': initial_point[1], 'width': width, 'height': height}
+    response = requests.get(url, params=params)
+    return json.loads(response.text)
+    
 def capture_screen(initial_point: Tuple[int, int]=(0, 0), width: int=600, height: int=600, base_dir: str='./data/', filename: str='screenshot.png') -> None:
     # 保存先を確認
     if os.path.exists(base_dir) and os.path.isfile(base_dir):
